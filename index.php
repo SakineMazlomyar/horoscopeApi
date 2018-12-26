54e68524667838030b94d2e860dff1efdc94a433
SakineMazlomyar <mazlomyar12@gmail.com>
2018-12-26 20:45:42 +0100

    addedForm

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Document</title>
+        <script src="./add/addHoroscope.php"></script>
+    </head>
+    <body>
+      
+        <form method='post' action='./update/updateHoroscope.php'>
+            <label>Skriv ditt födelsenummer:</label><br>
+            <input type="date" name="date"><br>
+
+            <input type='submit' name='addHoroscope' value='Spara mitt horoskop'><br>
+            <input type='submit' name='updateHoroscope' value='Uppdatera mitt horoskop'><br>
+            <input type='submit' name='deleteHoroscope' value='Radera mitt horoskop'>
+    
+       </form><br>
+
+       <?php
+        include("./view/viewHoroscope.php")
+       ?>
+ 
+        
+    </body>
+</html>
