ce34316e40b2f6f78bdbae5ae1adaba1d1bb8eb6
SakineMazlomyar <mazlomyar12@gmail.com>
2018-12-31 10:48:27 +0100

    addedScripts

--- index.php
+++ index.php
@@ -5,16 +5,21 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>Document</title>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  
+        <script src="./add/addHoroscope.js"></script>
+        <script src="./delete/deleteHoroscope.js"></script>
+        <script src="./update/updateHoroscope.js"></script>
     </head>
     <body>
       
-        <form method='post' action='./update/updateHoroscope.php'>
+        <form  method='post'>
             <label>Skriv ditt födelsenummer:</label><br>
             <input type="date" name="date"><br>
+            <input type="submit" class="add" name='addHoroscope' formaction="./add/addHoroscope.php" value="Spara mitt horoskop"><br>
+            <input type="submit" class="delete" name='deleteHoroscope' formaction="./delete/deleteHoroscope.php" value="Radera mitt horoskop"><br>
+            <input type="submit" class="update" name='updateHoroscope' formaction="./update/updateHoroscope.php" value="uppdatera mitt horoskop"><br>
 
-            <input type='submit' name='addHoroscope' value='Spara mitt horoskop'><br>
-            <input type='submit' name='updateHoroscope' value='Uppdatera mitt horoskop'><br>
-            <input type='submit' name='deleteHoroscope' value='Radera mitt horoskop'>
     
        </form><br>
 
