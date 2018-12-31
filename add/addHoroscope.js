e3a4ce3a45ea525fab5ee97f839bab3e324f1fb3
SakineMazlomyar <mazlomyar12@gmail.com>
2018-12-31 10:48:14 +0100

    workingOn

--- add/addHoroscope.js
+++ add/addHoroscope.js
@@ -1,4 +1,29 @@
-function addHoroscope(){
+$(document).ready(function(){
   
-    console.log("ss")
-}
+    $(".add").click(function(event){
+      event.preventDefault();
+      
+
+        $.ajax({
+            type:'post',
+            dataType:'json',
+            url:'index.php',
+            data:$(this).serialize(),
+            success: function(data){
+        
+                    console.log(data)
+                /* if(data.success){
+                    console.log(data.success)
+                }else{
+                    console.log('no mail')
+                } */
+            },
+            error: function(data, error) {
+                console.log( data +  error)
+            }
+        });
+        
+        
+    })
+})
+    
