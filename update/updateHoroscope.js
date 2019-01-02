56185e2d2c8c0200540fff496aeb7a11a58cfbd1
SakineMazlomyar <mazlomyar12@gmail.com>
2019-01-02 13:33:56 +0100

    addedUpdate

--- update/updateHoroscope.js
+++ update/updateHoroscope.js
@@ -1,28 +1,24 @@
-$(document).ready(function(){
-  
-    $(".update").click(function(event){
-      event.preventDefault();
-      
-      
-        $.ajax({
-            type:'post',
-            dataType:'json',
-            url:'./update/updateHoroscope.php',
-            data:$(this).serialize(),
-            success: function(data){
-        
-                    console.log(data)
-                /* if(data.success){
-                    console.log(data.success)
-                }else{
-                    console.log('no mail')
-                } */
-            },
-            error: function(data, error) {
-                console.log( data +  error)
+
+
+function updateHoroscopeContent(url){
+    var formInfo= $("#formInfo").serialize();
+    console.log(formInfo)
+    $.ajax({
+        type:'put',
+        dataType:'json',
+        url:'./update/updateHoroscope.php',
+        formInfo: formInfo,
+        success: function(userData){
+    
+            if(userData){
+                console.log(userData)
+            }else{
+                console.log('ingen data')
             }
-        });
-        
-        
-    })
-})
+        },
+        error: function(userData, error) {
+            console.log( userData +  error)
+        }
+    });
+
+}
