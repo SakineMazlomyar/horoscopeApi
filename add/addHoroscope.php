e62617a977b110b62210744fcfc6b98a7775ba23
SakineMazlomyar <mazlomyar12@gmail.com>
2019-01-07 10:53:11 +0100

    finishedAdd

--- add/addHoroscope.php
+++ add/addHoroscope.php
@@ -8,54 +8,61 @@ header("Content-Type: application/json");
 if(isset($_SESSION['horoscopeAndDate'])){
     $result = true;
     echo json_encode($result);
+
+/* test for outout
+$dateOfBirth = $_POST['dateBirth'];
+    print_r($dateOfBirth); */
     return;
     
 }else{
-  
-    $dateOfBirth = $_POST['dateBirth'];
-    
-    $montheOfBirth = substr($dateOfBirth,5,2);
-    $dayOfBirth= substr($dateOfBirth,8); 
+    if(isset($_POST['dateBirth'])){
 
-    
-    
-    $databasJson = file_get_contents("./horoscopes.json");
-    $databas = json_decode($databasJson);
-   
-    
-    
-    foreach($databas->horoscope as $horoscope){
+        $dateOfBirth = $_POST['dateBirth'];
+      
         
-        //we loop throw first month in horoscope array and compare day and first month
-        for($s=0; $s<count($horoscope->amountDaysOfFirstMonth); $s++){
-            if($horoscope->amountDaysOfFirstMonth[$s] == $dayOfBirth && $horoscope->firstMonth == $montheOfBirth){
-                $_SESSION['horoscopeAndDate'] =  $horoscope->nameHoroscope . $dateOfBirth;
-                
-                $result = true;
-                echo json_encode($result);
-                return;
-                
-                
-            }
-         
+        $montheOfBirth = substr($dateOfBirth,5,2);
+        $dayOfBirth= substr($dateOfBirth,8); 
+    
+        
+        
+        $databasJson = file_get_contents("./horoscopes.json");
+        $databas = json_decode($databasJson);
+       
+        
+        
+        foreach($databas->horoscope as $horoscope){
             
-        }
- 
-            //we loop throw second month in horoscope array and compare days and second month
-        for($j=0; $j<count($horoscope->amountDaysOfsecondMonth); $j++){
-            if($horoscope->amountDaysOfsecondMonth[$j] == $dayOfBirth && $horoscope->secondMonth == $montheOfBirth){
-                $_SESSION['horoscopeAndDate'] =  $horoscope->nameHoroscope . $dateOfBirth ;
-                $result = true;
-                echo json_encode($result);
-                return;
+            //we loop throw first month in horoscope array and compare day and first month
+            for($s=0; $s<count($horoscope->amountDaysOfFirstMonth); $s++){
+                if($horoscope->amountDaysOfFirstMonth[$s] == $dayOfBirth && $horoscope->firstMonth == $montheOfBirth){
+                    $_SESSION['horoscopeAndDate'] =  $horoscope->nameHoroscope . $dateOfBirth;
+                    
+                    $result = true;
+                    echo json_encode($result);
+                    return;
+                    
+                    
+                }
+             
                 
             }
+     
+                //we loop throw second month in horoscope array and compare days and second month
+            for($j=0; $j<count($horoscope->amountDaysOfsecondMonth); $j++){
+                if($horoscope->amountDaysOfsecondMonth[$j] == $dayOfBirth && $horoscope->secondMonth == $montheOfBirth){
+                    $_SESSION['horoscopeAndDate'] =  $horoscope->nameHoroscope . $dateOfBirth ;
+                    $result = true;
+                    echo json_encode($result);
+                    return;
+                    
+                }
+            }
         }
+        //when there is no such date
+        $result = false;
+        echo json_encode($result);
+        return; 
     }
-    //when there is no such date
-    $result = false;
-    echo json_encode($result);
-    return; 
     
     
 }
