95f023a2b1f7e12e2eba37e1178e7d6f45cc1bbe
SakineMazlomyar <mazlomyar12@gmail.com>
2019-01-08 14:34:21 +0100

    styledForm

--- index.php
+++ index.php
@@ -6,17 +6,18 @@
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>Document</title>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  
+        <link rel="stylesheet" href="./css/style.css">
         <script src="./add/addHoroscope.js"></script>
         <script src="./delete/deleteHoroscope.js"></script>
         <script src="./update/updateHoroscope.js"></script>
         <script src="./view/viewHoroscope.js"></script>
+
     </head>
     <body onload="content()">
       
         <form id="formInfo">
-            <label>Skriv ditt födelsenummer:</label><br>
-            <input type="date" name="dateBirth"><br>
+            <label>Skriv ditt födelsenummer:</label>
+            <input type="date" name="dateBirth">
         
             <input type="submit" value="save" onclick="return saveHoroscope('./add/addHoroscope.php')">
             <input type="submit" value="update" onclick="return updateHoroscopeContent('./update/updateHoroscope.php')">
