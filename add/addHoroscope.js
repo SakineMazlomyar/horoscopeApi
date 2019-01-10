43540d0a6bbfe4c5aadcb91bda8e47d119205a60
SakineMazlomyar <mazlomyar12@gmail.com>
2019-01-10 18:54:47 +0100

    changedToArrow

--- add/addHoroscope.js
+++ add/addHoroscope.js
@@ -1,14 +1,13 @@
 function saveHoroscope(url){
     var data= $("#formInfo").serialize();
-    console.log(data)
+    //console.log(data)
     $.ajax({
         type:'post',
         dataType:'json',
         url:'./add/addHoroscope.php',
         data: data,
-        success: function(dat){
-    
-            if(dat){
+        success: (dat) => {
+            if (dat) {
                 var div = document.querySelector("div#content");
                 div.innerHTML = dat;
                 content()
@@ -19,11 +18,11 @@ function saveHoroscope(url){
                 content()
             }
         },
-        error: function(dat, error) {
-            console.log( dat +  error)
-        }
+        error: (dat, error) => {console.log( dat +  error)}
     });
 
+
+    //to stop form from submitting
     return false;
 
 }
