bab1588609fdbccaa29ef26be2bcf91008b28d18
SakineMazlomyar <mazlomyar12@gmail.com>
2019-01-10 14:57:12 +0100

    addedComments

--- add/addHoroscope.php
+++ add/addHoroscope.php
@@ -6,6 +6,7 @@ session_start();
 header("Content-Type: application/json");
 
 if(isset($_SESSION['horoscopeAndDate'])){
+    // We check if there is alreay a saved horoscope and prevent it to rewrite.
     $result = true;
     echo json_encode($result);
 
@@ -32,7 +33,7 @@ $dateOfBirth = $_POST['dateBirth'];
         
         foreach($databas->horoscope as $horoscope){
             
-            //we loop throw first month in horoscope array and compare day and first month
+            //we loop throw first month in horoscope array and compare the day and the first month with the databirth
             for($s=0; $s<count($horoscope->amountDaysOfFirstMonth); $s++){
                 if($horoscope->amountDaysOfFirstMonth[$s] == $dayOfBirth && $horoscope->firstMonth == $montheOfBirth){
                     $_SESSION['horoscopeAndDate'] =  $horoscope->nameHoroscope . $dateOfBirth;
@@ -47,7 +48,7 @@ $dateOfBirth = $_POST['dateBirth'];
                 
             }
      
-                //we loop throw second month in horoscope array and compare days and second month
+                //we loop throw second month in horoscope array and compare the day and the second month with the datebirth
             for($j=0; $j<count($horoscope->amountDaysOfsecondMonth); $j++){
                 if($horoscope->amountDaysOfsecondMonth[$j] == $dayOfBirth && $horoscope->secondMonth == $montheOfBirth){
                     $_SESSION['horoscopeAndDate'] =  $horoscope->nameHoroscope . $dateOfBirth ;
@@ -58,7 +59,7 @@ $dateOfBirth = $_POST['dateBirth'];
                 }
             }
         }
-        //when there is no such date
+        //when we can not count the horoscope(when we do not have the date in data) and can not be saved
         $result = false;
         echo json_encode($result);
         return; 
